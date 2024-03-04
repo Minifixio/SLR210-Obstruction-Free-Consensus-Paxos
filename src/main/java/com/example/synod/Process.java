6055d4de1928a23141a33f1ea88491481fb1883d
adrien-gtd <adrienadrien0606@gmail.com>
2024-03-04 16:33:57 +0100

    feat: added intitialisation phase for the synod algorithm

--- src/main/java/com/example/synod/Process.java
+++ src/main/java/com/example/synod/Process.java
@@ -5,19 +5,26 @@ import akka.actor.Props;
 import akka.actor.UntypedAbstractActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.japi.Pair;
+
 import com.example.synod.message.Launch;
 import com.example.synod.message.Membership;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Process extends UntypedAbstractActor {
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);// Logger attached to actor
 
-    private int n;//number of processes
-    private int i;//id of current process
-    private Membership processes;//other processes' references
+    private int n;// number of processes
+    private int i;// id of current process
+    private Membership processes;// other processes' references
     private Boolean proposal;
     private int ballot;
+    private int readballot;
+    private int imposeballot;
+    private Boolean estiamte;
+    private ArrayList<Pair<Boolean, Integer>> states;
 
     /**
      * Static method to create an actor
@@ -29,13 +36,27 @@ public class Process extends UntypedAbstractActor {
     public Process(int n, int i) {
         this.n = n;
         this.i = i;
-        this.ballot = i-n;
+        this.ballot = i - n;
+        this.proposal = null;
+        this.readballot = 0;
+        this.imposeballot = i - n;
+        this.estiamte = null;
+        this.initState();
+
+    }
+
+    private void initState() {
+        this.states = new ArrayList<Pair<Boolean, Integer>>();
+        for (int index = 0; index < this.n; index++) {
+            this.states.add(new Pair<Boolean, Integer>(null, 0));
+        }
     }
 
     private void propose(Boolean v) {
-        log.info(this + " - propose("+ v+")");
+        log.info(this + " - propose(" + v + ")");
         proposal = v;
         ballot += n;
+        initState();
     }
 
     public void onReceive(Object message) throws Throwable {
